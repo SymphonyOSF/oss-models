ed361b7889b2391468481a3a95f46f1f5a834ba8
bruceskingle <bruce.skingle@symphony.com>
2020-10-15 08:26:51 +0100

    dummy pom

--- core-model/src/test/java/com/symphony/oss/models/core/canon/facade/SplitUserId.java
+++ core-model/src/test/java/com/symphony/oss/models/core/canon/facade/SplitUserId.java
@@ -10,8 +10,13 @@ public class SplitUserId
 {
   public static void main(String argv[])
   {
-    PodAndUserId podAndUserId = PodAndUserId.newBuilder().build(68719539845L);
+    PodId        expectedPodId  = PodId.newBuilder().build(5006);
+    PodAndUserId podAndUserId   = PodAndUserId.newBuilder().build(68719578631L);
+    PodAndUserId expectedUserId =  PodAndUserId.newBuilder().build(expectedPodId, podAndUserId.getUserId());
     
     System.out.println("User " + podAndUserId + " is local user " + podAndUserId.getUserId() + " in pod " + podAndUserId.getPodId());
+    System.out.println("Expected PodId is " + expectedPodId);
+    System.out.println("ExternalUserId = " + expectedUserId);
+   
   }
 }
